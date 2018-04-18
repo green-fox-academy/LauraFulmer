e28af5a0f475369da8ae2624972c296f60a3ddc1
LauraFulmer <laurahajnalka@gmail.com>
2018-04-18 17:10:28 +0200

    atHome

---
+++ week-02/day-1/src/DrawPyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DrawPyramid {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // pyramid like this:
+        //
+        //
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //
+        // The pyramid should have as many lines as the number was
+
+        System.out.println("How big pyramid would you like to have?");
+
+        Scanner scanner = new scanner(System.in);
+            int pyramid = scanner.next.Int();
+
+            for (int i = 1; i <= pyramid; i++) {
+                
+            }
+
+    }
+}
---
+++ week-02/day-1/src/DrawTriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+
+        System.out.println("How big triangle would you like to have?");
+        Scanner scanner = new Scanner(System.in);
+            int triangle = scanner.nextInt();
+
+        String star = "*";
+        for (int i = 1; i <= triangle; i++ ) {
+              System.out.println(star);
+              star += "*";
+        }
+
+    }
+}
