3ace8bc1f268ef0a332c4411c4a23199112bd7c5
LauraFulmer <laurahajnalka@gmail.com>
2018-04-19 08:52:15 +0200

    Arrays

---
+++ week-03/src/Swap.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        // - Create an array variable named `abc`
+        //   with the following content: `["first", "second", "third"]`
+        // - Swap the first and the third element of `abc`
+
+        String[] abc = {"first", "second", "third"};
+
+        String something = abc[2];
+        abc[2] = abc[0];
+        abc[0] = something;
+
+        System.out.println(Arrays.toString(abc));
+    }
+}
