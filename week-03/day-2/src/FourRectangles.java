d9fed1f5f11db052bd868cfd2148f7fc540a8532
LauraFulmer <laurahajnalka@gmail.com>
2018-04-20 20:54:46 +0200

    Drawing classwork

---
+++ week-03/day-2/src/FourRectangles.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        Class color = ("Color.GREEN", "Color.BLUE", "Color.YELLOW", "Color.RED");
+        int[] x1 = {20, 80, 140, 200};
+        int[] y1 = {20, 20, 20, 20};
+        int[] x2 = {70, 130, 190, 250};
+        int[] y2 = {70, 70, 70, 70};
+
+        drawRectangles(graphics, x1, y1, x2, y2);
+     }
+
+     public static void drawRectangles(Graphics graphics, Class color, int[] x1, int[] y1, int[] x2, int[] y2) {
+         for (int i = 0; i <= 3; i++) {
+             graphics.setColor(color[i]);
+             graphics.drawRect(x1[i], y1[i], x2[i], y2[i]);
+         }
+     }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
---
+++ week-03/day-2/src/GoToCenter.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a line from that point to the center of the canvas.
+        // Draw 3 lines with that function. Use loop for that.
+
+    int[] x = {23, 100, 300};
+    int[] y = {54, 230, 310};
+
+    drawingLines(graphics, x, y);
+    }
+
+    public static void drawingLines(Graphics graphics, int[] x, int[] y) {
+
+        graphics.setColor(Color.RED);
+
+        for (int i = 0; i <= 2; i++) {
+            graphics.drawLine(x[i], y[i], 160, 171);
+        }
+    }
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
