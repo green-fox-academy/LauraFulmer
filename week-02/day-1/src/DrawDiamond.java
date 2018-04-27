6a2b6970123ac6ee083eef2206aa0f05fd504e3c
LauraFulmer <laurahajnalka@gmail.com>
2018-04-27 20:24:20 +0200

    Finally done with DrawDiamond

--- week-02/day-1/src/DrawDiamond.java
+++ week-02/day-1/src/DrawDiamond.java
@@ -25,7 +25,28 @@ public class DrawDiamond {
                 scanner = new Scanner(System.in);
                 diamond = scanner.nextInt();
             }
+        for (int i = 0; i <= diamond / 2; i++) {
+            for (int j = diamond / 2 + 1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0; i < diamond; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
 
-
+        for (int i = 0; i <= diamond / 2 - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= (diamond / 2) - (2 * i -1); k++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
