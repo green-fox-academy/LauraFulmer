8ef9f4eb84ece1eea0c03837a654d81e0d6862fa
LauraFulmer <laurahajnalka@gmail.com>
2018-05-02 20:48:15 +0200

    BlogPost

---
+++ week-05/day-1/BlogPost/src/Blogpost.java
@@ -0,0 +1,13 @@
+public class Blogpost {
+    String authorName;
+    String title;
+    String text;
+    String publicationDate;
+
+    public Blogpost(String authorName, String title, String text, String publicationDate) {
+        this.authorName = authorName;
+        this.title = title;
+        this.text = text;
+        this.publicationDate = publicationDate;
+    }
+}
