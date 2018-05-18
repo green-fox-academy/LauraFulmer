219d0c24052f5907f09640c92fe6f14b9e3a1f7d
LauraFulmer <laurahajnalka@gmail.com>
2018-05-18 20:44:17 +0200

    Exercise2

--- week-07/day-2/src/Exercise2.java
+++ week-07/day-2/src/Exercise2.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Exercise2 {
     public static void main(String[] args) {
-        
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+        double averageOfNumbers = numbers.stream()
+                .filter(number -> number % 2 == 1)
+                .mapToInt(number -> number)
+                .average()
+                .getAsDouble();
+
+        System.out.println(averageOfNumbers);
+
     }
 }
